47b7c81ee2eec4897e7563888a51608758c28876
FELIPE ANDRES RODRIGUEZ TORRES <felipe.rodriguez73@inacapmail.cl>
2019-06-04 21:59:59 -0400

    Add classes to Data

--- Datos/AccesoDatosCamionero.cs
+++ Datos/AccesoDatosCamionero.cs
@@ -10,46 +10,54 @@ namespace Datos
     class AccesoDatosCamionero
     {
         //INSERTAR DATOS DEL CAMIONERO
-        public int InsertCamionero(int id_producto, string producto, string descripcion)
+        public int InsertCamionero(int rut, string dv, string nombre, int telefono, string direccion, int salario, string poblacion)
         {
             SqlCommand cmd = MetodoDatos.CrearComando();
-            cmd.CommandText = "insert into TBL_PRODUCTO  (id_producto, producto, descripcion) values (@id_producto, @producto, @descripcion)";
+            cmd.CommandText = "insert into Camionero (Rut, Dv, Nombre, Telefono, Direccion, Salario, Poblacion) values (@rut, @dv, @nombre, @telefono, @direccion, @salario, @poblacion)";
 
-            cmd.Parameters.Add("@id_producto", id_producto);
-            cmd.Parameters.Add("@producto", producto);
-            cmd.Parameters.Add("@descripcion", descripcion);
+            cmd.Parameters.Add("@rut", rut);
+            cmd.Parameters.Add("@dv", dv);
+            cmd.Parameters.Add("@nombre", nombre);
+            cmd.Parameters.Add("@telefono", telefono);
+            cmd.Parameters.Add("@direccion", direccion);
+            cmd.Parameters.Add("@salario", salario);
+            cmd.Parameters.Add("@poblacion", poblacion);
 
             return MetodoDatos.ManejoTablas(cmd);
         }
         //MODIFICAR DATOS DEL CAMIONERO
-        public int UpdateCamionero(int id_producto, string producto, string descripcion)
+        public int UpdateCamionero(int rut, string dv, string nombre, int telefono, string direccion, int salario, string poblacion)
         {
             SqlCommand cmd = MetodoDatos.CrearComando();
-            cmd.CommandText = "update TBL_PRODUCTO set producto=@producto, descripcion=@descripcion where  id_producto = @id_producto";
+            cmd.CommandText = "update Camionero set Dv=@dv, Nombre=@nombre, Telefono=@telefono, Direccion=@direccion, Salario=@salario, Poblacion=@poblacion where Rut=@rut";
 
-            cmd.Parameters.Add("@id_producto", id_producto);
-            cmd.Parameters.Add("@producto", producto);
-            cmd.Parameters.Add("@descripcion", descripcion);
+            cmd.Parameters.Add("@rut", rut);
+            cmd.Parameters.Add("@dv", dv);
+            cmd.Parameters.Add("@nombre", nombre);
+            cmd.Parameters.Add("@telefono", telefono);
+            cmd.Parameters.Add("@direccion", direccion);
+            cmd.Parameters.Add("@salario", salario);
+            cmd.Parameters.Add("@poblacion", poblacion);
 
             return MetodoDatos.ManejoTablas(cmd);
         }
         //ELIMINAR DATOS DEL CAMIONERO
-        public int DeleteCamionero(int id_producto)
+        public int DeleteCamionero(int rut)
         {
             SqlCommand cmd = MetodoDatos.CrearComando();
-            cmd.CommandText = "delete from TBL_PRODUCTO where  id_producto = @id_producto";
+            cmd.CommandText = "delete from Camionero where Rut=@rut";
 
-            cmd.Parameters.Add("@id_producto", id_producto);
+            cmd.Parameters.Add("@rut", rut);
 
             return MetodoDatos.ManejoTablas(cmd);
         }
         //BUSCAR Y LISTAR DATOS DEL CAMIONERO
-        public static DataTable ListCamionero(int id_producto)
+        public static DataTable ListCamionero(int rut)
         {
             SqlCommand cmd = MetodoDatos.CrearComando();
-            cmd.CommandText = "select * from TBL_PRODUCTO where  id_producto = @id_producto";
+            cmd.CommandText = "select * from Camionero where Rut = @rut";
 
-            cmd.Parameters.Add("@id_producto", id_producto);
+            cmd.Parameters.Add("@rut", rut);
             return MetodoDatos.ConsultaDatos(cmd);
         }
     }
--- Datos/AccesoDatosPaquete.cs
+++ Datos/AccesoDatosPaquete.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 
@@ -7,6 +9,55 @@ namespace Datos
 {
     class AccesoDatosPaquete
     {
-        //Generar CRUD para cada tabla en archivos separados
+        //INSERTAR DATOS DEL PAQUETE
+        public int InsertPaquete(int cod_paquete, string descripcion, string destinatario, string direcdestinatario, int codcomuna, int rut)
+        {
+            SqlCommand cmd = MetodoDatos.CrearComando();
+            cmd.CommandText = "insert into Paquete (Cod_Paquete, Descripcion, Destinatario, Direc_Destinatario, Cod_Comuna, Rut) values (@cod_paquete, @descripcion, @destinatario, @direcdestinatario, @codcomuna, @rut)";
+
+            cmd.Parameters.Add("@cod_paquete", cod_paquete);
+            cmd.Parameters.Add("@descripcion", descripcion);
+            cmd.Parameters.Add("@destinatario", destinatario);
+            cmd.Parameters.Add("@direcdestinatario", direcdestinatario);
+            cmd.Parameters.Add("@codcomuna", codcomuna);
+            cmd.Parameters.Add("@rut", rut);
+
+            return MetodoDatos.ManejoTablas(cmd);
+        }
+        //MODIFICAR DATOS DEL PAQUETE
+        public int UpdatePaquete(int cod_paquete, string descripcion, string destinatario, string direcdestinatario, int codcomuna, int rut)
+        {
+            SqlCommand cmd = MetodoDatos.CrearComando();
+            cmd.CommandText = "update Paquete set Descripcion=@descripcion, Destinatario=@destinatario, Direc_Destinatario=@direcdestinatario, Cod_Comuna=@codcomuna, Rut=@rut where Cod_Paquete=@cod_paquete";
+
+            cmd.Parameters.Add("@cod_paquete", cod_paquete);
+            cmd.Parameters.Add("@descripcion", descripcion);
+            cmd.Parameters.Add("@destinatario", destinatario);
+            cmd.Parameters.Add("@direcdestinatario", direcdestinatario);
+            cmd.Parameters.Add("@codcomuna", codcomuna);
+            cmd.Parameters.Add("@rut", rut);
+
+            return MetodoDatos.ManejoTablas(cmd);
+        }
+        //ELIMINAR DATOS DEL PAQUETE
+        public int DeletePaquete(int cod_paquete)
+        {
+            SqlCommand cmd = MetodoDatos.CrearComando();
+            cmd.CommandText = "delete from Paquete where Cod_Paquete=@cod_paquete";
+
+            cmd.Parameters.Add("@cod_paquete", cod_paquete);
+
+            return MetodoDatos.ManejoTablas(cmd);
+        }
+        //BUSCAR Y LISTAR DATOS DEL CAMIONERO
+        public static DataTable ListCamionero(int cod_paquete)
+        {
+            SqlCommand cmd = MetodoDatos.CrearComando();
+            cmd.CommandText = "select * from Paquete where Cod_Paquete=@cod_paquete";
+
+            cmd.Parameters.Add("@cod_paquete", cod_paquete);
+
+            return MetodoDatos.ConsultaDatos(cmd);
+        }
     }
 }
